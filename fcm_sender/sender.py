4d3842f75305fb3870121fb20c464436815f181f
Josep <jap8@alumnes.udl.cat>
2017-02-06 13:07:27 +0200

    Extracted the creation of the headers.

--- fcm_sender/sender.py
+++ fcm_sender/sender.py
@@ -7,8 +7,14 @@ class Sender():
     default_topic = ""
     api_key = ""
 
+    def create_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'key={}'.format(self.api_key)
+        }
+
     def send_message(self, message, topic=default_topic):
         try:
-            requests.post(url=fcm_url, data={}, headers={'Content-Type': 'application/json', 'Authorization': 'key=' + self.api_key})
+            requests.post(url=fcm_url, data={}, headers=self.create_headers())
         except:
             pass
