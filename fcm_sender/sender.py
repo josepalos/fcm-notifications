69bf73c23e82e73541e618268cd7df7f0913ef15
Josep <jap8@alumnes.udl.cat>
2017-02-07 16:48:20 +0200

    Extracted the creation of the data to create_data method.

--- fcm_sender/sender.py
+++ fcm_sender/sender.py
@@ -13,13 +13,16 @@ class Sender():
             'Authorization': 'key={}'.format(self.api_key)
         }
 
+    def create_data(self, message, topic):
+        return {
+            'to': '/topic/{}'.format(topic),
+            'data': {
+                'message': message
+            }
+        }
+
     def send_message(self, message, topic=default_topic):
         try:
-            requests.post(url=fcm_url, data={
-                'to': '/topic/{}'.format(topic),
-                'data': {
-                    'message': message
-                }
-            }, headers=self.create_headers())
+            requests.post(url=fcm_url, data=self.create_data(message, topic), headers=self.create_headers())
         except:
             pass
